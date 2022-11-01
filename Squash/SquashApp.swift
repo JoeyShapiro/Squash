3afddba6c86b50af8d7f6d82f681524d817f6c05
JoeyShapiro <joeyashapiro@gmail.com>
2022-11-01 17:21:23 -0500

    Initial Commit

---
+++ Squash/SquashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquashApp.swift
+//  Squash
+//
+//  Created by Joey Shapiro on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SquashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
